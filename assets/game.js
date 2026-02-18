d681bf37089f32a7b625bf23cd44b38dab386f33
jakubforman <me@jakubforman.eu>
2026-02-18 12:40:01 +0100

    Add game reset functionality and update current player display logic

--- assets/game.js
+++ assets/game.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const endGame = document.getElementById('end-game')
+    const currentPlayerElement = document.getElementById('current-player')
+
     // vyberu boxy
     const boxes = document.querySelectorAll('.box')
     // Právě táhnoucí hráč (X nebo O)
     let currentPlayer = 'X';
+    currentPlayerElement.innerText = currentPlayer;
 
     // projdu každý boc
     boxes.forEach(box => {
@@ -15,6 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
             box.classList.add(currentPlayer === 'X' ? 'cross' : 'circle')
             // měním hráče
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            currentPlayerElement.innerText = currentPlayer;
+        })
+    })
+
+    // ukončení hry / zahájení nové
+    endGame.addEventListener('click', () => {
+        currentPlayer = 'X';
+        boxes.forEach(box => {
+            box.classList.remove('cross', 'circle');
         })
     })
 })
